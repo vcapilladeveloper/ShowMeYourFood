648eb877e92e10baa6a7fc72727658df4740888f
Victor Capilla Borrego <victor.capilla@gmail.com>
2019-02-24 18:52:29 +0100

    Develop Connection Module

---
+++ ConnectionModule/ConnectionEngine.swift
@@ -0,0 +1,39 @@
+//
+//  ConnectionEngine.swift
+//  ConnectionModule
+//
+//  Created by Victor Capilla Developer on 23/02/2019.
+//  Copyright © 2019 VCapillaDev. All rights reserved.
+//
+
+// Manager to make the request to the API.
+// In this project, all requests are GET type.
+open class ConnectionEngine: NSObject {
+    
+    static var defaultSession: URLSession!
+    static var dataGetTask: URLSessionDataTask?
+    public typealias RequestResult = (_ data: Any?, _ error: Bool) -> Void
+    
+    public class func getResult(_ url: URL, _ method: String = "GET", completionHandler: @escaping RequestResult) {
+        
+        dataGetTask?.cancel()
+        defaultSession = URLSession(configuration: .default)
+        var urlRequest = URLRequest(url: url)
+        
+        //urlRequest.httpMethod = method
+        dataGetTask = defaultSession.dataTask(with: url) { data, response, error in
+            if let response = response as? HTTPURLResponse { print(response.statusCode) }
+            defer { self.dataGetTask = nil }
+            if let error = error {
+                completionHandler(data, true)
+            } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode >= 200, response.statusCode <= 300 {
+                print(data)
+                completionHandler(data, false)
+            } else {
+                completionHandler(nil, true)
+            }
+        }
+        defer {dataGetTask?.resume()}
+    }
+    
+}
---
+++ ConnectionModule/ConnectionModule.h
@@ -0,0 +1,19 @@
+//
+//  ConnectionModule.h
+//  ConnectionModule
+//
+//  Created by Victor Capilla Developer on 23/02/2019.
+//  Copyright © 2019 VCapillaDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConnectionModule.
+FOUNDATION_EXPORT double ConnectionModuleVersionNumber;
+
+//! Project version string for ConnectionModule.
+FOUNDATION_EXPORT const unsigned char ConnectionModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectionModule/PublicHeader.h>
+
+
